47368f91c3ada16582690f764c6995a38ceb2d82
SC\ashtyne.madsen <ashtyne.madsen@my.simpson.edu>
2017-02-07 09:26:54 -0600

    Add new item button opens hidden form and save changes button prints to console

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -2,20 +2,6 @@
  * Created by ashtyne.madsen on 2/2/2017.
  */
 
-function addItems() {
-    for (var i = 0; i < json_result.length; i++) {
-        var id = json_result[i].id;
-        var firstName = json_result[i].first;
-        var lastName = json_result[i].last;
-        var email = json_result[i].email;
-        var phone = json_result[i].phone;
-        var phoneDash = phone.substr(0,3) + '-' + phone.substr(3,3) + '-' + phone.substr(6,4);
-        var birthday = json_result[i].birthday;
-}
-
-var Button1 = $('#addItem');
-    Button1.on("click", addItems);
-
 // Main Javascript File
 function updateTable() {
     // Here's where your code is going to go.
@@ -49,5 +35,38 @@ function updateTable() {
     })
 }
 
+// Called when "Add Item" button is clicked
+function showDialogAdd() {
+
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+function saveChangesWords() {
+    console.log("The changes were saved!");
+}
+
 // Call your code.
 updateTable();
+
+// There's a button in the form with the ID "addItem"
+// Associate the function showDialogAdd with it.
+var addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+var saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", saveChangesWords)
