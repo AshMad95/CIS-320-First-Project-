6b96220195125a8906cbdf7da4c76d8fe187d922
Ashtyne Madsen <ashtyne.madsen@my.simpson.edu>
2017-02-13 14:41:20 -0600

    Added regular expressions for validation and colors and glyphs for validation (Finished Lab 5)

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -52,23 +52,224 @@ function showDialogAdd() {
     $('#phone').val("");
     $('#birthday').val("");
 
+    // Remove style for outline of form field
+    $('#idDiv').removeClass("has-error");
+    $('#idGlyph').removeClass("glyphicon-remove");
+    $('#idDiv').removeClass("has-success");
+    $('#idGlyph').removeClass("glyphicon-ok");
+    $('#firstNameDiv').removeClass("has-error");
+    $('#firstNameGlyph').removeClass("glyphicon-remove");
+    $('#firstNameDiv').removeClass("has-success");
+    $('#firstNameGlyph').removeClass("glyphicon-ok");
+    $('#lastNameDiv').removeClass("has-error");
+    $('#lastNameGlyph').removeClass("glyphicon-remove");
+    $('#lastNameDiv').removeClass("has-success");
+    $('#lastNameGlyph').removeClass("glyphicon-ok");
+    $('#emailDiv').removeClass("has-error");
+    $('#emailGlyph').removeClass("glyphicon-remove");
+    $('#emailDiv').removeClass("has-success");
+    $('#emailGlyph').removeClass("glyphicon-ok");
+    $('#phoneDiv').removeClass("has-error");
+    $('#phoneGlyph').removeClass("glyphicon-remove");
+    $('#phoneDiv').removeClass("has-success");
+    $('#phoneGlyph').removeClass("glyphicon-ok");
+    $('#birthdayDiv').removeClass("has-error");
+    $('#birthdayGlyph').removeClass("glyphicon-remove");
+    $('#birthdayDiv').removeClass("has-success");
+    $('#birthdayGlyph').removeClass("glyphicon-ok");
+
     // Show the hidden dialog
     $('#myModal').modal('show');
 }
 
-function validateFirstName(event) {
-    var v1 = $('#firstName').val();
-    var reg = (/[a-zA-Z] + ('[a-zA-Z])?[a-zA-Z]*)){30}$/;
+function validation(event) {
+    var idValidate = $('#id').val();
+    var reg = /^[0-9]{1,10}$/;
 
-    if (reg.test(v1)) {
-        console.log('valid')
-    } else {
-        console.log('invalid')
+    if (reg.test(idValidate)) {
+        // Set style for outline of form field
+        $('#idDiv').removeClass("has-error");
+        $('#idDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#idGlyph').removeClass("glyphicon-remove");
+        $('#idGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('idStatus').val("(success)");
+        console.log('Valid ID')
+    }
+    else {
+        // Set style for outline of form field
+        $('#idDiv').removeClass("has-success");
+        $('#idDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#idGlyph').removeClass("glyphicon-ok");
+        $('#idGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('idStatus').val("(error)");
+        console.log('Invalid ID')
+    }
+
+
+    var firstNameValidate = $('#firstName').val();
+    var reg = /^[a-zA-Z' -]{1,30}$/;
+
+    if (reg.test(firstNameValidate)) {
+        // Set style for outline of form field
+        $('#firstNameDiv').removeClass("has-error");
+        $('#firstNameDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#firstNameGlyph').removeClass("glyphicon-remove");
+        $('#firstNameGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('firstNameStatus').val("(success)");
+        console.log('Valid First Name')
+    }
+    else {
+        // Set style for outline of form field
+        $('#firstNameDiv').removeClass("has-success");
+        $('#firstNameDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#firstNameGlyph').removeClass("glyphicon-ok");
+        $('#firstNameGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('firstNameStatus').val("(error)");
+        console.log('Invalid First Name')
+    }
+
+
+    var lastNameValidate = $('#lastName').val();
+    var reg = /^[a-zA-Z' -]{1,30}$/;
+
+    if (reg.test(lastNameValidate)) {
+        // Set style for outline of form field
+        $('#lastNameDiv').removeClass("has-error");
+        $('#lastNameDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#lastNameGlyph').removeClass("glyphicon-remove");
+        $('#lastNameGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('lastNameStatus').val("(success)");
+        console.log('Valid Last Name')
+    }
+    else {
+        // Set style for outline of form field
+        $('#lastNameDiv').removeClass("has-success");
+        $('#lastNameDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#lastNameGlyph').removeClass("glyphicon-ok");
+        $('#lastNameGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('lastNameStatus').val("(error)");
+        console.log('Invalid Last Name')
+    }
+
+
+    var emailValidate = $('#email').val();
+    var reg = /^[a-zA-Z0-9_-]{1,30}@[a-zA-Z]{1,30}\.[a-zA-Z]{1,4}$/;
+
+    if (reg.test(emailValidate)) {
+        // Set style for outline of form field
+        $('#emailDiv').removeClass("has-error");
+        $('#emailDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#emailGlyph').removeClass("glyphicon-remove");
+        $('#emailGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('emailStatus').val("(success)");
+        console.log('Valid Email')
+    }
+    else {
+        // Set style for outline of form field
+        $('#emailDiv').removeClass("has-success");
+        $('#emailDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#emailGlyph').removeClass("glyphicon-ok");
+        $('#emailGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('emailStatus').val("(error)");
+        console.log('Invalid Email')
+    }
+
+    var phoneValidate = $('#phone').val();
+    var reg = /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/;
+
+    if (reg.test(phoneValidate)) {
+        // Set style for outline of form field
+        $('#phoneDiv').removeClass("has-error");
+        $('#phoneDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#phoneGlyph').removeClass("glyphicon-remove");
+        $('#phoneGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('phoneStatus').val("(success)");
+        console.log('Valid Phone Number')
+    }
+    else {
+        // Set style for outline of form field
+        $('#phoneDiv').removeClass("has-success");
+        $('#phoneDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#phoneGlyph').removeClass("glyphicon-ok");
+        $('#phoneGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('phoneStatus').val("(error)");
+        console.log('Invalid Phone Number')
+    }
+
+
+    var birthdayValidate = $('#birthday').val();
+    var reg = /^(19|20)[1-9]{2}[- /](0[1-9]|1[012])[- /](0[1-9]|[12][0-9]|3[01])$/;
+
+    if (reg.test(birthdayValidate)) {
+        // Set style for outline of form field
+        $('#birthdayDiv').removeClass("has-error");
+        $('#birthdayDiv').addClass("has-success");
+
+        // Set the icon for the form field
+        $('#birthdayGlyph').removeClass("glyphicon-remove");
+        $('#birthdayGlyph').addClass("glyphicon-ok");
+
+        // Put in the field used by screen readers
+        $('birthdayStatus').val("(success)");
+        console.log('Valid Birthday')
+    }
+    else {
+        // Set style for outline of form field
+        $('#birthdayDiv').removeClass("has-success");
+        $('#birthdayDiv').addClass("has-error");
+
+        // Set the icon for the form field
+        $('#birthdayGlyph').removeClass("glyphicon-ok");
+        $('#birthdayGlyph').addClass("glyphicon-remove");
+
+        // Put in the field used by screen readers
+        $('birthdayStatus').val("(error)");
+        console.log('Invalid Birthday')
     }
 }
 
 function saveChangesWords() {
-    validateFirstName()
+    validation()
     console.log("The changes were saved!");
 }
 
